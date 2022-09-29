aea1c0f7c40bb06bc5e0f889b054fc99dfbe6448
MahmudaDisa <dishamirza.h@gmail.com>
2022-09-29 12:33:00 +0200

    responsive done

---
+++ src/utilities/fakedb.js
@@ -0,0 +1,48 @@
+// use local storage to manage cart data
+const addToDb = id => {
+    // let shoppingCart = {};
+    const shopping = getCart();
+    // add quantity
+    const quantity = shopping[id];
+    if (quantity) {
+        const newQuantity = quantity + 1;
+        shopping[id] = newQuantity;
+    }
+    else {
+        shopping[id] = 1;
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(shopping));
+}
+//find your cart first
+const getCart = () => {
+    let shoppingCart = {};
+
+    //get the shopping cart from local storage
+    const storedCart = localStorage.getItem('shopping-cart');
+    if (storedCart) {
+        shoppingCart = JSON.parse(storedCart);
+    }
+    return shoppingCart;
+}
+
+const removeFromDb = id => {
+    const storedCart = localStorage.getItem('shopping-cart');
+    if (storedCart) {
+        const shoppingCart = JSON.parse(storedCart);
+        if (id in shoppingCart) {
+            delete shoppingCart[id];
+            localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+        }
+    }
+}
+
+const deleteShoppingCart = () => {
+    localStorage.removeItem('shopping-cart');
+}
+
+export {
+    addToDb,
+    removeFromDb,
+    deleteShoppingCart,
+    getCart
+}
